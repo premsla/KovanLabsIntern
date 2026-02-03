e71afca96d428dba64608be4c5aa449f8cc20878
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-03 13:32:31 +0530

    Implemented covariant return type example

--- src/com/kovanLabs/intern/InheritancePolymorphism/Covariant.java
+++ src/com/kovanLabs/intern/InheritancePolymorphism/Covariant.java
@@ -1,4 +1,24 @@
 package com.kovanLabs.intern.InheritancePolymorphism;
-
+class Shape1 {
+    Shape1 getShape() {
+        System.out.println("Returning Shape");
+        return this;
+    }
+}
+class Circle1 extends Shape1 {
+    @Override
+    Circle1 getShape() {
+        System.out.println("Returning Circle");
+        return this;
+    }
+    void drawCircle() {
+        System.out.println("Drawing Circle");
+    }
+}
 public class Covariant {
+    public static void main(String[] args) {
+        Circle1 c = new Circle1();
+        Circle1 c2 = c.getShape();
+        c2.drawCircle();
+    }
 }
