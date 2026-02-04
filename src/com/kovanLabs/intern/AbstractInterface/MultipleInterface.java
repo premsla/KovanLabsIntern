9d987d928808efe1d296bf44b7795cca187486ff
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-04 17:57:46 +0530

    Added Multiple interface with example

---
+++ src/com/kovanLabs/intern/AbstractInterface/MultipleInterface.java
@@ -0,0 +1,36 @@
+package com.kovanLabs.intern.AbstractInterface;
+interface camera{
+    default void start(){
+        System.out.print("camera..");
+    }
+    void takePhoto();
+}
+interface MusicPlayer{
+    void start();
+    void playMusic();
+}
+interface Phone {
+    void start();
+    void makecall();
+}
+public class MultipleInterface implements camera,MusicPlayer,Phone{
+    @Override
+    public void start() {
+        System.out.println("SmartPhone");
+    }
+    @Override
+    public void takePhoto() {
+        System.out.println("Taking photo");
+    }
+    @Override
+    public void playMusic() {
+        System.out.println("Playing music");
+    }
+    @Override
+    public void makecall() {
+        System.out.println("Making a call");
+    }
+}
+
+
+
