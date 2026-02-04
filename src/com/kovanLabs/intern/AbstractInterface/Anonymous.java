99cb59eff215a0c5ae47aecfed816ddc0098093c
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-04 17:56:41 +0530

    Added Anonymous problems

---
+++ src/com/kovanLabs/intern/AbstractInterface/Anonymous.java
@@ -0,0 +1,14 @@
+package com.kovanLabs.intern.AbstractInterface;
+@FunctionalInterface
+interface Plugin{
+    void execute();
+}
+class CoreSystem{
+    void runPlugin(Plugin[] plugins){
+        for(Plugin plugin:plugins){
+            plugin.execute();
+        }
+    }
+}
+public class Anonymous {
+}
