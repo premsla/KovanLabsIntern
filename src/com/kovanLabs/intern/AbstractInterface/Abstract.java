1f9cdd775883593b09874cd463de2beb2c510cf1
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-04 17:55:35 +0530

    Added abstract wth example

---
+++ src/com/kovanLabs/intern/AbstractInterface/Abstract.java
@@ -0,0 +1,58 @@
+package com.kovanLabs.intern.AbstractInterface;
+abstract class Payment{
+    abstract void pay();
+    abstract void refund();
+}
+abstract class GooglePay extends Payment{
+    void pay(){
+        System.out.println("Payment Done...");
+    }
+}
+class updateGooglePay extends GooglePay{
+    void refund(){
+        System.out.println("Refund is done..");
+    }
+}
+public class Abstract {
+    public static void main(String[] args){
+//        Payment obj=new updateGooglePay();
+//        obj.pay();
+//        obj.refund();
+//          Plugin[] plugins = new Plugin[] {
+//                new Plugin() {
+//                    public void execute() {
+//                        System.out.println("Authentication plugin executed");
+//                    }
+//                },
+//                new Plugin() {
+//                    public void execute() {
+//                        System.out.println("Logging plugin executed");
+//                    }
+//                },
+//                new Plugin() {
+//                    public void execute() {
+//                        System.out.println("Payment plugin executed");
+//                    }
+//                },
+//                new Plugin() {
+//                    public void execute() {
+//                        System.out.println("Refund plugin executed");
+//                    }
+//                },
+//        };
+//
+//        Plugin[] plugins=new Plugin[]{
+//                ()-> System.out.println("Authentication Plugin is executed"),
+//                ()-> System.out.println("Login Plugin is Executed"),
+//                ()-> System.out.println("Payment Plugin is executed")
+//        };
+//        CoreSystem coreSystem = new CoreSystem();
+//        coreSystem.runPlugin(plugins);
+        MultipleInterface mi=new MultipleInterface();
+        mi.start();
+        mi.takePhoto();
+        mi.playMusic();
+        mi.makecall();
+    }
+}
+
