5c58d9a9048c89412ca8db1755a801250d676755
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-05 15:48:09 +0530

    Added Inheritance and compositions

---
+++ src/com/kovanLabs/intern/CompositionOverInheritance/Computer.java
@@ -0,0 +1,57 @@
+package com.kovanLabs.intern.CompositionOverInheritance;
+import javax.swing.plaf.synth.SynthTextAreaUI;
+class Processor{
+    private String brand;
+    private int core;
+    public Processor(String brand,int core){
+        this.brand=brand;
+        this.core=core;
+    }
+    public String getDetails(){
+        return brand +" "+core;
+    }
+}
+class Ram{
+    private int size;
+    public Ram(int size){
+        this.size=size;
+    }
+    public int getSize(){
+        return size;
+    }
+}
+class Storage{
+    private int size;
+    private String type;
+    public Storage(int size,String type){
+        this.size=size;
+        this.type=type;
+    }
+    public String getStorage(){
+        return size+" GB "+type;
+    }
+}
+public class Computer {
+    private Processor processor;
+    private Ram ram;
+    private Storage storage;
+    public Computer(Processor processor,Ram ram,Storage storage){
+        this.processor=processor;
+        this.ram=ram;
+        this.storage=storage;
+    }
+    public void upgrade(Ram newRam){
+        if(newRam.getSize()>this.ram.getSize()){
+            this.ram=newRam;
+            System.out.println("Ram ugraded to : "+newRam.getSize());
+        }else{
+            System.out.println("upgraded fails");
+        }
+    }
+    public void display(){
+        System.out.println("COMPUTER");
+        System.out.println("Processor : "+processor.getDetails());
+        System.out.println("Ram : "+ram.getSize());
+        System.out.println("Storage : "+storage.getStorage());
+    }
+}
---
+++ src/com/kovanLabs/intern/CompositionOverInheritance/RelationshipIS.java
@@ -0,0 +1,64 @@
+package com.kovanLabs.intern.CompositionOverInheritance;
+class Person{
+    private int id;
+    private String name;
+    private int age;
+    public Person(int id,String name,int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+}
+class Employee extends Person{
+    private int salary;
+    public Employee(int id,String name,int age,int salary){
+        super(id,name,age);
+        this.salary=salary;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public void setSalary(int salary){
+        this.salary=salary;
+    }
+    public void display(){
+        System.out.println("Id :"+getId());
+        System.out.println("Name :"+getName());
+        System.out.println("Age :"+getAge());
+        System.out.println("Salary :"+getSalary());
+    }
+}
+public class RelationshipIS {
+    public static void main(String[] args){
+        Employee emp=new Employee(1,"Prema",21,100000);
+        emp.display();
+        Processor processor=new Processor("Ryzen",5);
+        Ram oldram=new Ram(8);
+        Storage storage=new Storage(512,"SSD");
+        Computer computer=new Computer(processor,oldram,storage);
+        computer.display();
+        Ram newRam=new Ram(16);
+        computer.upgrade(newRam);
+
+
+    }
+
+}
