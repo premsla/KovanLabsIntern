7ee6d42e5794901cd5398b7966860b0bd85d7e1e
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-15 12:56:17 +0530

    added file handling task

---
+++ src/com/kovanLabs/intern/filehandling/DirectoryWalker.java
@@ -0,0 +1,25 @@
+package com.kovanLabs.intern.filehandling;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+public class DirectoryWalker {
+    public static void main(String[] args) {
+        Path startPath = Paths.get("C:\\Users\\Asus\\IdeaProjects\\KovanLabsIntern\\src\\com\\kovanLabs\\intern"); // change your folder
+        try (Stream<Path> paths = Files.walk(startPath)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> {
+                        try {
+                            long size = Files.size(path);
+                            System.out.println(path + " -> " + size + " bytes");
+                        } catch (IOException e) {
+                            System.out.println("Error reading file: " + path);
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
