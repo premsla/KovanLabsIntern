6b9e159eaf8f95c4d47e0b416d152291600612ed
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-16 15:40:13 +0530

    Added file Hnadling task

--- src/com/kovanLabs/intern/filehandling/FileExample.java
+++ src/com/kovanLabs/intern/filehandling/FileExample.java
@@ -1,10 +1,12 @@
 package com.kovanLabs.intern.filehandling;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
 public class FileExample {
     public static void main(String[] args) {
         Path filePath = Paths.get("C:\\Users\\Asus\\IdeaProjects\\Recurtion\\src\\com\\kovanLabs\\intern\\filehandling\\example.txt");
