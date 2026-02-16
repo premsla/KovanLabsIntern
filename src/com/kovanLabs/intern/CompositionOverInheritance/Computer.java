b5b562648b260abf91e909f36e390814a80f7ce7
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-16 15:34:06 +0530

    Changes Done add string format

--- src/com/kovanLabs/intern/CompositionOverInheritance/Computer.java
+++ src/com/kovanLabs/intern/CompositionOverInheritance/Computer.java
@@ -1,57 +1,69 @@
 package com.kovanLabs.intern.CompositionOverInheritance;
-import javax.swing.plaf.synth.SynthTextAreaUI;
-class Processor{
+
+class Processor {
     private String brand;
     private int core;
-    public Processor(String brand,int core){
-        this.brand=brand;
-        this.core=core;
+
+    public Processor(String brand, int core) {
+        this.brand = brand;
+        this.core = core;
     }
-    public String getDetails(){
-        return brand +" "+core;
+
+    public String getDetails() {
+        return String.format("%s %d cores", brand, core);
     }
 }
-class Ram{
+
+class Ram {
     private int size;
-    public Ram(int size){
-        this.size=size;
+
+    public Ram(int size) {
+        this.size = size;
     }
-    public int getSize(){
+
+    public int getSize() {
         return size;
     }
 }
-class Storage{
+
+class Storage {
     private int size;
     private String type;
-    public Storage(int size,String type){
-        this.size=size;
-        this.type=type;
+
+    public Storage(int size, String type) {
+        this.size = size;
+        this.type = type;
     }
-    public String getStorage(){
-        return size+" GB "+type;
+
+    public String getStorage() {
+        return String.format("%d GB %s", size, type);
     }
 }
+
 public class Computer {
     private Processor processor;
     private Ram ram;
     private Storage storage;
-    public Computer(Processor processor,Ram ram,Storage storage){
-        this.processor=processor;
-        this.ram=ram;
-        this.storage=storage;
+
+    public Computer(Processor processor, Ram ram, Storage storage) {
+        this.processor = processor;
+        this.ram = ram;
+        this.storage = storage;
     }
-    public void upgrade(Ram newRam){
-        if(newRam.getSize()>this.ram.getSize()){
-            this.ram=newRam;
-            System.out.println("Ram ugraded to : "+newRam.getSize());
-        }else{
+
+    public void upgrade(Ram newRam) {
+        if (newRam.getSize() > this.ram.getSize()) {
+            this.ram = newRam;
+            System.out.println("Ram ugraded to : " + newRam.getSize());
+        } else {
             System.out.println("upgraded fails");
         }
     }
-    public void display(){
+
+    public void display() {
         System.out.println("COMPUTER");
-        System.out.println("Processor : "+processor.getDetails());
-        System.out.println("Ram : "+ram.getSize());
-        System.out.println("Storage : "+storage.getStorage());
+        System.out.println("Processor : " + processor.getDetails());
+        System.out.println("Ram : " + ram.getSize());
+        System.out.println("Storage : " + storage.getStorage());
     }
 }
