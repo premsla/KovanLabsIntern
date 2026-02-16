0409d97a325785988342936ad88c6c43eb3939e7
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-16 15:32:54 +0530

    Changes instanceof Done

--- src/com/kovanLabs/intern/Collections/TestHashMap.java
+++ src/com/kovanLabs/intern/Collections/TestHashMap.java
@@ -1,22 +1,26 @@
 package com.kovanLabs.intern.Collections;
+
 import java.util.*;
+
 class Person {
     int id;
     String name;
+
     Person(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
     @Override
     public boolean equals(Object obj) {
-        Person p =(Person)obj;
+        if (!(obj instanceof Person)) return false;
+        Person p = (Person) obj;
         if (this == obj) return true;
         return this.id == p.id;
     }
+
     public int hashCode() {
         return Integer.hashCode(id);
     }
 }
-public class TestHashMap {
 
-}
