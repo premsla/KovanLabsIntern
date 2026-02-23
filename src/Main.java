f79f6d8c035296909079446ef8b5364f931d95f5
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-23 22:47:21 +0530

    changes done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import com.kovanLabs.intern.exceptionhandling.Resource;
 import com.kovanLabs.intern.exceptionhandling.*;
 import com.kovanLabs.intern.stream.Stream;
@@ -7,28 +6,29 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
             try(Resource r=new Resource()) {
                 r.divide();
             }
-        Resource r=new Resource();
-        r.divide();
-        DataProcessingException.FileProcesser fp=new DataProcessingException.FileProcesser();
+            Resource r=new Resource();
+            r.divide();
+            DataProcessingException.FileProcesser fp=new DataProcessingException.FileProcesser();
             try {
                 fp.processFile("data.txt");
             }catch (DataProcessingException e){
                 System.out.println(e.getMessage());
             }
 
-        List<Integer> number=new Random()
-                .ints(100,0,1000)
-                .boxed()
-                .collect(Collectors.toList());
+            List<Integer> number=new Random()
+                    .ints(100,0,1000)
+                    .boxed()
+                    .collect(Collectors.toList());
             IntSummaryStatistics stats= Stream.calculate(number);
-        System.out.println("Count : " + stats.getCount());
-        System.out.println("Sum : " + stats.getSum());
-        System.out.println("Average : " + stats.getAverage());
-        System.out.println("Min : " + stats.getMin());
-        System.out.println("Max : " + stats.getMax());
+            System.out.println("Count : " + stats.getCount());
+            System.out.println("Sum : " + stats.getSum());
+            System.out.println("Average : " + stats.getAverage());
+            System.out.println("Min : " + stats.getMin());
+            System.out.println("Max : " + stats.getMax());
+        }
     }
-}
+
