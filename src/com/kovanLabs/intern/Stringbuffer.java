e3c07335bd46bfe43a8d91cf8f49c8ecbd063968
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-02-23 22:52:58 +0530

    added stringbuffer task

---
+++ src/com/kovanLabs/intern/Stringbuffer.java
@@ -0,0 +1,38 @@
+package com.kovanLabs.intern;
+
+class MyThread extends Thread {
+    StringBuilder sb;
+
+    MyThread(StringBuilder sb) {
+        this.sb = sb;
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+              sb.append(Thread.currentThread().getName() + "\n");
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
+
+public class Stringbuffer {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        MyThread t1 = new MyThread(sb);
+        MyThread t2 = new MyThread(sb);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+
+        System.out.println(sb);
+        System.out.println();
+    }
+}
